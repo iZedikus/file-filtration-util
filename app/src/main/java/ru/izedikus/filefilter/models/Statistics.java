616bf6327127b7d7e949d77ad694f15dedd84d65
iZedikus <izedikus555@mail.ru>
2025-07-29 11:10:00 +0700

    docs: Statistics

--- app/src/main/java/ru/izedikus/filefilter/models/Statistics.java
+++ app/src/main/java/ru/izedikus/filefilter/models/Statistics.java
@@ -3,21 +3,39 @@ package ru.izedikus.filefilter.models;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 
+/**
+ * Statistical summary for parsed input files.
+ *
+ * @param countInt     number of parsed integers
+ * @param countFloat   number of parsed decimals
+ * @param countString  number of parsed strings
+ * @param sumInt       total sum of all parsed integers
+ * @param minInt       minimal parsed integer
+ * @param maxInt       maximal parsed integer
+ * @param avgInt       arithmetic mean of parsed integers
+ * @param sumFloat     total sum of all parsed decimals
+ * @param minFloat     minimal parsed decimal
+ * @param maxFloat     maximal parsed decimal
+ * @param avgFloat     arithmetic mean of parsed decimals
+ * @param minStringLen length of the shortest parsed string
+ * @param maxStringLen length of the longest parsed string
+ */
 public record Statistics(
-  Long countInt,
-  Long countFloat,
-  Long countString,
+        Long countInt,
+        Long countFloat,
+        Long countString,
 
-  BigInteger sumInt,
-  BigInteger minInt,
-  BigInteger maxInt,
-  BigDecimal avgInt,
+        BigInteger sumInt,
+        BigInteger minInt,
+        BigInteger maxInt,
+        BigDecimal avgInt,
 
-  BigDecimal sumFloat,
-  BigDecimal minFloat,
-  BigDecimal maxFloat,
-  BigDecimal avgFloat,
+        BigDecimal sumFloat,
+        BigDecimal minFloat,
+        BigDecimal maxFloat,
+        BigDecimal avgFloat,
 
-  int minStringLen,
-  int maxStringLen
-  ) {}
+        int minStringLen,
+        int maxStringLen
+) {
+}
